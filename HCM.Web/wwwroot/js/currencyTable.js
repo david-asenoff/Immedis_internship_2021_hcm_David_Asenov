c4f415988f81ef28b0b7f8cb7a08fe562bc80c4b
David Asenov <david.asenoff@gmail.com>
2021-08-07 12:04:09 +0300

    Add Currency controller, corresponding views and js

---
+++ HCM.Web/wwwroot/js/currencyTable.js
@@ -0,0 +1,25 @@
+﻿var currencyId;
+var abbreviation;
+var description;
+var deletedOn;
+
+$(document).ready(function () {
+    $('#myTable').DataTable({
+        "scrollX": true, 
+        "scrollCollapse": true,
+        "paging": true,
+        dom: 'frtipP',
+        responsive: true,
+    });
+});
+
+$('#deleteModal,#restoreModal').on('show.bs.modal', function (e) {
+    currencyId = $(e.relatedTarget).data('currency-id');
+    abbreviation = $(e.relatedTarget).data('currency-abbreviation');
+    description = $(e.relatedTarget).data('currency-description');
+    deletedOn = $(e.relatedTarget).data('currency-deletedon');
+    $(e.currentTarget).find('input[name="Id"]').val(currencyId);
+    $(e.currentTarget).find('input[name="Abbreviation"]').val(abbreviation);
+    $(e.currentTarget).find('input[name="Description"]').val(description);
+    $(e.currentTarget).find('input[name="DeletedOn"]')?.val(deletedOn);
+});
