3c249f7e89dacdd3233214ce395748807cefdf85
David Asenov <david.asenoff@gmail.com>
2021-08-07 12:06:10 +0300

    Add Gender, Marital status and Parental status controllers, corresponding views and js

---
+++ HCM.Web/Areas/Administrator/Controllers/GenderController.cs
@@ -0,0 +1,105 @@
+﻿namespace HCM.Web.Areas.Administrator.Controllers
+{
+    using HCM.Data.Common;
+    using HCM.Services.Contracts;
+    using HCM.Web.ViewModels.Gender;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+
+    [Area("Administrator")]
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    public class GenderController : Controller
+    {
+        private readonly IGenderService genderService;
+
+        public GenderController(IGenderService genderService)
+        {
+            this.genderService = genderService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var result = await genderService.GetAllAsync();
+            TempData["LoadingSuccessMessage"] = "Genders are loaded";
+            return View(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            return this.View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+
+            var model = await genderService.GetAsync(id);
+            TempData["SuccessMessage"] = "Gender is loaded";
+
+            return this.View(model);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(GenderEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await genderService.EditAsync(model);
+                TempData["SuccessMessage"] = "Gender is edited";
+                return RedirectToAction("Index", "Gender");
+            }
+
+            TempData["ErrorMessage"] = "Gender is not edited. Invalid data.";
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(GenderDeleteViewModel model)
+        {
+            if (ModelState.IsValid)
+            { 
+                var dbGender = await genderService.DeleteAsync(model);
+                TempData["SuccessMessage"] = "Gender is deleted";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Gender is not deleted. Invalid data.";
+            }
+            TempData.Keep();
+            return RedirectToAction("Index", "Gender");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Restore(GenderRestoreViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dbGender = await genderService.RestoreAsync(model);
+                TempData["SuccessMessage"] = "Gender is restored";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Gender is not restored. Invalid data.";
+            }
+            TempData.Keep();
+            return RedirectToAction("Index", "Gender");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(GenderAddViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await genderService.AddAsync(model);
+                TempData["SuccessMessage"] = "Gender is added";
+                return RedirectToAction("Index", "Gender");
+            }
+
+            TempData["ErrorMessage"] = "Gender is not added. Invalid data.";
+
+            return this.View(model);
+        }
+    }
+}
---
+++ HCM.Web/wwwroot/js/maritalStatusTable.js
@@ -0,0 +1,22 @@
+﻿var maritalStatusId;
+var type;
+var deletedOn;
+
+$(document).ready(function () {
+    $('#myTable').DataTable({
+        "scrollX": true, 
+        "scrollCollapse": true,
+        "paging": true,
+        dom: 'frtipP',
+        responsive: true,
+    });
+});
+
+$('#editModal,#deleteModal,#restoreModal,#addModal').on('show.bs.modal', function (e) {
+    maritalStatusId = $(e.relatedTarget).data('maritalstatus-id');
+    type = $(e.relatedTarget).data('maritalstatus-type');
+    deletedOn = $(e.relatedTarget).data('maritalstatus-deletedon');
+    $(e.currentTarget).find('input[name="Id"]').val(maritalStatusId);
+    $(e.currentTarget).find('input[name="Type"]').val(type);
+    $(e.currentTarget).find('input[name="DeletedOn"]')?.val(deletedOn);
+});
