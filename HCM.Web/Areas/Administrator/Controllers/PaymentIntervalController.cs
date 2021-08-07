b2a404934697b1003ea3459dd9176124581c60eb
David Asenov <david.asenoff@gmail.com>
2021-08-07 12:08:48 +0300

    Add Position, Payment interval and Training controllers, corresponding views and js

---
+++ HCM.Web/Areas/Administrator/Controllers/PaymentIntervalController.cs
@@ -0,0 +1,104 @@
+﻿namespace HCM.Web.Areas.Administrator.Controllers
+{
+    using HCM.Data.Common;
+    using HCM.Services.Contracts;
+    using HCM.Web.ViewModels.PaymentInterval;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+
+    [Area("Administrator")]
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    public class PaymentIntervalController : Controller
+    {
+        private readonly IPaymentIntervalService paymentIntervalService;
+
+        public PaymentIntervalController(IPaymentIntervalService paymentIntervalService)
+        {
+            this.paymentIntervalService = paymentIntervalService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var result = await paymentIntervalService.GetAllAsync();
+            TempData["LoadingSuccessMessage"] = "Payment Intervals are loaded";
+            return View(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            return this.View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+
+            var model = await paymentIntervalService.GetAsync(id);
+            TempData["SuccessMessage"] = "PaymentInterval is loaded";
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(PaymentIntervalEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await paymentIntervalService.EditAsync(model);
+                TempData["SuccessMessage"] = "PaymentInterval is edited";
+                return RedirectToAction("Index", "PaymentInterval");
+            }
+
+            TempData["ErrorMessage"] = "PaymentInterval is not edited. Invalid data.";
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(PaymentIntervalDeleteViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await paymentIntervalService.DeleteAsync(model);
+                TempData["SuccessMessage"] = "PaymentInterval is deleted";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "PaymentInterval is not deleted. Invalid data.";
+            }
+            TempData.Keep();
+            return RedirectToAction("Index", "PaymentInterval");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Restore(PaymentIntervalRestoreViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dbPaymentInterval = await paymentIntervalService.RestoreAsync(model);
+                TempData["SuccessMessage"] = "PaymentInterval is restored";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "PaymentInterval is not restored. Invalid data.";
+            }
+            TempData.Keep();
+            return RedirectToAction("Index", "PaymentInterval");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(PaymentIntervalAddViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await paymentIntervalService.AddAsync(model);
+                TempData["SuccessMessage"] = "PaymentInterval is added";
+                return RedirectToAction("Index", "PaymentInterval");
+            }
+
+            TempData["ErrorMessage"] = "PaymentInterval is not added. Invalid data.";
+
+            return this.View(model);
+        }
+    }
+}
---
+++ HCM.Web/Areas/Administrator/Controllers/TrainingController.cs
@@ -0,0 +1,107 @@
+﻿using HCM.Data.Common;
+using HCM.Services.Contracts;
+using HCM.Web.ViewModels.Training;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HCM.Web.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    public class TrainingController : Controller
+    {
+        private readonly ITrainingService trainingService;
+
+        public TrainingController(ITrainingService trainingService)
+        {
+            this.trainingService = trainingService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var result = await trainingService.GetAllAsync();
+            TempData["LoadingSuccessMessage"] = "Trainings are loaded";
+            return View(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(TrainingEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dbTraining = await trainingService.EditAsync(model);
+                TempData["SuccessMessage"] = "Training is edited";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Training is not edited. Invalid data.";
+            }
+            TempData.Keep();
+            return RedirectToAction("Index", "Training");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(TrainingDeleteViewModel model)
+        {
+            if (ModelState.IsValid)
+            { 
+                var dbTraining = await trainingService.DeleteAsync(model);
+                TempData["SuccessMessage"] = "Training is deleted";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Training is not deleted. Invalid data.";
+            }
+            TempData.Keep();
+            return RedirectToAction("Index", "Training");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Restore(TrainingRestoreViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dbTraining = await trainingService.RestoreAsync(model);
+                TempData["SuccessMessage"] = "Training is restored";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Training is not restored. Invalid data.";
+            }
+            TempData.Keep();
+            return RedirectToAction("Index", "Training");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(TrainingAddViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dbTraining = await trainingService.AddAsync(model);
+                TempData["SuccessMessage"] = "Training is added";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Training is not added. Invalid data.";
+            }
+            TempData.Keep();
+            return RedirectToAction("Index", "Training");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+
+            var model = await trainingService.GetAsync(id);
+            TempData["SuccessMessage"] = "Training is loaded";
+
+            return this.View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            return this.View();
+        }
+    }
+}
