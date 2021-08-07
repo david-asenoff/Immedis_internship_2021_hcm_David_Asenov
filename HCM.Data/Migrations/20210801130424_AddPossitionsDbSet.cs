e52597eac2b7885b2d9d07ae09b4888eb3273a70
David Asenov <david.asenoff@gmail.com>
2021-08-07 11:27:07 +0300

    Add migrations
    - CV and portrait are not required,
    - Employee has position,
    - Total hours and work hours will be stored as integer.
    
    To do: Change the name of Db table from Possitions to Positions.

---
+++ HCM.Data/Migrations/20210801130424_AddPossitionsDbSet.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HCM.Data.Migrations
+{
+    public partial class AddPossitionsDbSet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EmployeeContracts_Possition_PossitionId",
+                table: "EmployeeContracts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Possition",
+                table: "Possition");
+
+            migrationBuilder.RenameTable(
+                name: "Possition",
+                newName: "Possitions");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Possitions",
+                table: "Possitions",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EmployeeContracts_Possitions_PossitionId",
+                table: "EmployeeContracts",
+                column: "PossitionId",
+                principalTable: "Possitions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EmployeeContracts_Possitions_PossitionId",
+                table: "EmployeeContracts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Possitions",
+                table: "Possitions");
+
+            migrationBuilder.RenameTable(
+                name: "Possitions",
+                newName: "Possition");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Possition",
+                table: "Possition",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EmployeeContracts_Possition_PossitionId",
+                table: "EmployeeContracts",
+                column: "PossitionId",
+                principalTable: "Possition",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
