cb83e1fe9f16cd581ddd8f2eda5454d336eac3dd
David Asenov <david.asenoff@gmail.com>
2021-08-07 11:34:24 +0300

    Create ExceptionMessages.cs

---
+++ HCM.Data.Common/ExceptionMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace HCM.Data.Common
+{
+    public static class ExceptionMessages
+    {
+        public const string CannotRestoreDeletedObject = "Cannot restore a non deleted  object";
+        public const string CannotEditDeletedObject = "Cannot edit a deleted  object";
+        public const string CannotDeletedAnAlreadyDeletedObject = "Cannot delete an already deleted  object";
+        public const string CannotCreateDublicateObject = "Cannot create dublicate object.";
+    }
+}
