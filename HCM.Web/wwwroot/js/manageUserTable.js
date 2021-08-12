7ca3db1ea3d4e621a24251087e0d65958b28c9e8
David Asenov <david.asenoff@gmail.com>
2021-08-12 11:38:35 +0300

    Fix role not rendering issue for Admin's manage user view

--- HCM.Web/wwwroot/js/manageUserTable.js
+++ HCM.Web/wwwroot/js/manageUserTable.js
@@ -1,9 +1,22 @@
-﻿$(document).ready(function () {
+﻿var genderId;
+var type;
+var deletedOn;
+
+$(document).ready(function () {
     $('#myTable').DataTable({
-        "scrollX": true, 
+        "scrollX": true,
         "scrollCollapse": true,
         "paging": true,
         dom: 'frtipP',
         responsive: true,
     });
+});
+
+$('#editModal,#deleteModal,#restoreModal,#addModal').on('show.bs.modal', function (e) {
+    genderId = $(e.relatedTarget).data('gender-id');
+    type = $(e.relatedTarget).data('gender-type');
+    deletedOn = $(e.relatedTarget).data('gender-deletedon');
+    $(e.currentTarget).find('input[name="Id"]').val(genderId);
+    $(e.currentTarget).find('input[name="Type"]').val(type);
+    $(e.currentTarget).find('input[name="DeletedOn"]')?.val(deletedOn);
 });
