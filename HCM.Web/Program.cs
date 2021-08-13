db3dfe73ce21f7cc4f19ee7dc060047bc5eb8491
David Asenov <david.asenoff@gmail.com>
2021-08-13 12:59:14 +0300

    Add Metrics for Prometheus and Grafana

--- HCM.Web/Program.cs
+++ HCM.Web/Program.cs
@@ -12,6 +12,17 @@ namespace HCM.Web
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseMetricsWebTracking()
+                //Use Prometheus text formatter
+                //.UseMetrics(options =>
+                //{
+                //    options.EndpointOptions = endpointsOptions =>
+                //    {
+                //        endpointsOptions.MetricsTextEndpointOutputFormatter = new MetricsPrometheusTextOutputFormatter();
+                //        endpointsOptions.MetricsEndpointOutputFormatter = new MetricsPrometheusTextOutputFormatter();
+                //        endpointsOptions.EnvironmentInfoEndpointEnabled = false;
+                //    };
+                //})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
