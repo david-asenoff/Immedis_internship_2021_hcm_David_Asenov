c38f75772888b8bd94a3581a64b896c50de74a0a
David Asenov <david.asenoff@gmail.com>
2021-08-13 17:17:04 +0300

    Fix the need of refresh when avatar is uploaded

--- HCM.Web/wwwroot/js/avatar.js
+++ HCM.Web/wwwroot/js/avatar.js
@@ -2,4 +2,11 @@
     var logoString = document.querySelector('input[name="ProfileAvatar"]').value;
     var img = document.getElementById('profileAvatar');
     img.src = logoString;
+    var logoString = document.querySelector('input[name="ProfileAvatar"]');
+    var fReader = new FileReader();
+    fReader.readAsDataURL(logoString.files[0]);
+    fReader.onloadend = function (event) {
+        var img = document.getElementById('profileAvatar');
+        img.src = event.target.result;
+    }
 }
